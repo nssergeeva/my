3d27d4bc13311fe29172462aafe23fffa5516b0e
Nadezda <Sergeevanadezda160@gmail.com>
2024-05-29 13:00:57 +0300

    ddd

---
+++ main.py
@@ -0,0 +1,43 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Задача «Длина слова».
+a = 'It is difficult very difficult'
+print(len(a))
+# Задача «Суммы и разности».
+first = 2256
+second = 1580
+
+summa = first + second
+diff = first - second
+print(summa, diff)
+# Задача «Среднее арифметическое».
+first = 30
+second = 27
+third = 9
+mean = (first + second + third)/3
+print(mean)
+# Задача «Простые строчки».
+first_string = "Вторник"
+second_string = "Понедельник"
+print("Понедельник" ",", "Вторник")
+# Задача «Сложная формула».
+a = 191919
+b = 121212
+c = 3
+f = ((a * b) + (b * c))** 3/2
+print(f)
