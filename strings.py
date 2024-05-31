7ae41e47dc9f4e746e1aa11d4265fa49bfea3cf8
nssergeeva <Sergeevanadezda160@gmail.com>
2024-05-31 22:00:18 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -3,4 +3,4 @@ number_of_hours_spent = 1.5
 course_name = 'Python'
 time_for_one_task = (number_of_hours_spent / the_number_of_completed_dz)
 print(time_for_one_task)
-print('Курс:', course_name, ",", 'всего задач:', the_number_of_completed_dz, ',', 'затрачено часов:', number_of_hours_spent,',', 'среднее время выполнения:', time_for_one_task,'.')
+print('Курс:', course_name, ",", 'всего задач:', the_number_of_completed_dz, ',', 'затрачено часов:', number_of_hours_spent,',', 'среднее время выполнения:', time_for_one_task,'часа', '.')
