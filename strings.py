a2c553a5d417a85b3aedd73c62fed7dcf9a6259f
nssergeeva <Sergeevanadezda160@gmail.com>
2024-06-02 15:43:11 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,6 +1,8 @@
-the_number_of_completed_dz = 12
-number_of_hours_spent = 1.5
-course_name = 'Python'
-time_for_one_task = (number_of_hours_spent / the_number_of_completed_dz)
-print(time_for_one_task)
-print('Курс:', course_name, ",", 'всего задач:', the_number_of_completed_dz, ',', 'затрачено часов:', number_of_hours_spent,',', 'среднее время выполнения:', time_for_one_task,'часа', '.')
+name = 'Nadya'
+print('Name:', name)
+age = 21
+print('Age:', age)
+age = 23
+print('New Age:', age)
+is_student = True
+print('Is Student:', is_student)
