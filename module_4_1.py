4cb5279a1f707a2283e2513ac9d368d6f5f57a44
nssergeeva <Sergeevanadezda160@gmail.com>
2024-06-30 17:39:12 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as k1
+from true_math import divide as k2
+result1 = k1(69,3)
+result2 = k1(3,0)
+result3 = k2(49,7)
+result4 = k2(15,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
