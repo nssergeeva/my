94c99d5d554e46053412c500f0c12f5a02040f58
nssergeeva <Sergeevanadezda160@gmail.com>
2024-07-14 12:14:45 +0300

    Add files via upload

--- module5hard.py
+++ module5hard.py
@@ -1,98 +1,82 @@
 import time
+class User:
+    def __init__(self, nickname: str, password: int, age: int):
+        self.nickname = nickname
+        self.password = password
+        self.age = age
+
+    def __str__(self):
+        return self.nickname
+
+
+class Video:
+    def __init__(self, title: str, duration: int, adult_mode: bool = False):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        self.adult_mode = adult_mode
+
+    def __eq__(self, other):
+        return self.title == other.title
+
+    def __contains__(self, item):  # item - string
+        return item in self.title  # 'aa' in 'aannn'
 
 
 class UrTube:
-    users = []
-    videos = []
-    current_user = None
-
-    def log_in(self, login: str, password: str) -> None:
-        """Метод log_in, который принимает на вход аргументы: login, password и пытается найти пользователя в users с
-        такими же логином и паролем. Если такой пользователь существует, то current_user меняется на найденного.
-        Помните, что password передаётся в виде строки, а сравнивается по хэшу."""
-        for user in self.users:
-            if login == user.nickname and password == user.password:
-                self.current_user = user
-
-    def register(self, nickname: str, password: str, age: int) -> None:
-        """Метод register, который принимает три аргумента: nickname, password, age, и добавляет пользователя в список,
-        если пользователя не существует (с таким же nickname). Если существует, выводит на экран:
-        "Пользователь {nickname} уже существует". После регистрации, вход выполняется автоматически."""
-        for user in self.users:
-            if nickname in user.nickname:
-                print(f"Пользователь {nickname} уже существует")
-                break
-        else:
-            user = User(nickname, password, age)
-            self.users.append(user)
-            self.log_out()
-            self.log_in(user.nickname, user.password)
-
-    def log_out(self):
-        """Метод log_out для сброса текущего пользователя на None."""
+    def __init__(self):
+        self.users = []
+        self.videos = []
         self.current_user = None
 
-    def add(self, *args):
-        """Метод add, который принимает неограниченное кол-во объектов класса Video и все добавляет в videos,
-        если с таким же названием видео ещё не существует. В противном случае ничего не происходит."""
-        for movie in args:
-            self.videos.append(movie)
+    def log_out(self):
+        self.current_user = None
 
-    def get_videos(self, text: str) -> list:
-        """Метод get_videos, который принимает поисковое слово и возвращает список названий всех видео,
-        содержащих поисковое слово. Следует учесть, что слово 'UrbaN' присутствует в строке
-        'Urban the best' (не учитывать регистр)."""
+    def log_in(self, nickname: str, password: int):
+        for user in self.users:
+            if nickname == user.nickname and password == user.password:
+                self.current_user = user
+                return
+
+    def register(self, nickname: str, password: int, age: int):
+        password = hash(password)
+        for user in self.users:
+            if nickname == user.nickname:
+                print(f"Пользователь {nickname} уже существует")
+                return
+        new_user = User(nickname, password, age)
+        self.users.append(new_user)
+        self.current_user = new_user
+        print(f"Пользователь {nickname} зарегистрирован и вошел в систему.")
+
+    def add(self, *args):  # args - tuple, внутри которого лежат Video
+        for movie in args:  # movie это объект класса Video
+            if movie.title not in [video.title for video in self.videos]:
+                self.videos.append(movie)
+
+    def get_videos(self, text: str):
         list_movie = []
         for video in self.videos:
             if text.upper() in video.title.upper():
                 list_movie.append(video.title)
         return list_movie
 
-    def watch_video(self, movie: str) -> None:
-        """Метод watch_video, который принимает название фильма, если не находит точного совпадения(вплоть до пробела),
-        то ничего не воспроизводится, если же находит, ведётся отчёт в консоль на какой секунде ведётся просмотр.
-        После текущее время просмотра данного видео сбрасывается."""
-        if self.current_user and self.current_user.age < 18:
-            print('Вам нет 18 лет, пожалуйста покиньте страницу')
-        elif self.current_user:
+    def watch_video(self, movie: str):
+        if self.current_user:
             for video in self.videos:
+                if self.current_user and self.current_user.age < 18:
+                    print('Вам нет 18 лет, пожалуйста покиньте страницу')
+                    return
                 if movie in video.title:
                     for i in range(1, 11):
                         print(i, end=' ')
                         time.sleep(1)
+                        video.time_now += 1
+                    video.time_now = 0
                     print('Конец видео')
+
         else:
             print('Войдите в аккаунт, чтобы смотреть видео')
-
-    def __str__(self):
-        return f"{self.videos}"
-
-
-class Video:
-    def __init__(self, title: str, duration: int, time_now: int = 0, adult_mode: bool = False):
-        self.title = title
-        self.duration = duration
-        self.time_now = time_now
-        self.adult_mode = adult_mode
-
-    def __str__(self):
-        return f"{self.title}"
-
-
-class User:
-    def __init__(self, nickname: str, password: str, age: int):
-        self.nickname = nickname
-        self.password = password
-        self.age = age
-
-    def __str__(self):
-        return f'{self.nickname}'
-
-    def __eq__(self, other):
-        return self.nickname == other.nickname
-
-    def __hash__(self):
-        return hash(self.password)
 
 
 if __name__ == '__main__':
@@ -100,18 +84,23 @@ if __name__ == '__main__':
     v1 = Video('Лучший язык программирования 2024 года', 200)
     v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
+    # Добавление видео
     ur.add(v1, v2)
 
+    # Проверка поиска
     print(ur.get_videos('лучший'))
-    print(ur.get_videos('ПРОГ'))
+print(ur.get_videos('ПРОГ'))
 
-    ur.watch_video('Для чего девушкам парень программист?')
-    ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-    ur.watch_video('Для чего девушкам парень программист?')
-    ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-    ur.watch_video('Для чего девушкам парень программист?')
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
 
-    ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-    print(ur.current_user)
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
 
-    ur.watch_video('Лучший язык программирования 2024 года!')
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')
